68ec75387b5c5f2a535483a7ec172d6368d28739
Willian <willfakri@gmail.com>
2016-01-31 15:52:54 -0200

    Passo 1 - Classes iniciais.

---
+++ HeadFirstCSharp.Cap7.Home/Business/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirstCSharp.Cap7.Home.Business
+{
+    public abstract class Location
+    {
+        public string Name { get; set; }
+        public object[] Exits { get; set; }
+
+        public void Description()
+        {
+
+        }
+    }
+}
---
+++ HeadFirstCSharp.Cap7.Home/Business/Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirstCSharp.Cap7.Home.Business
+{
+    public class Room : Location
+    {
+        public string Decoration { get; private set; }
+    }
+}
