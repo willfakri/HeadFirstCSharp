2a4dd55dea5fe80b53d57b80307d7c1705451512
Willian Fakri <wfakri@ciandt.com>
2016-02-02 13:09:13 -0200

    Backend

---
+++ HeadFirstCSharp.Cap7.Home/Business/OutsideWithDoor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirstCSharp.Cap7.Home.Business
+{
+    class OutsideWithDoor : Outside, IHasExteriorDoor
+    {
+        public OutsideWithDoor(bool hot, string name) : base(hot, name)
+        {
+        }
+
+        public string DoorDescription
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Location DoorLocation
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
